d787726aa6335a879d56eb33b1ebce8637c17003
madittman <mdtn@gmx.de>
2025-06-25 14:06:56 +0200

    Add black as formatter

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import grpc
 from protos import calculator_pb2, calculator_pb2_grpc
 
+
 def run(num1, num2):
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel("localhost:50051") as channel:
         stub = calculator_pb2_grpc.CalculatorStub(channel)
         response = stub.Add(calculator_pb2.AddRequest(num1=num1, num2=num2))
     print(f"Result: {response.result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Get user Input
     num1 = int(input("Please input num1: "))
     num2 = int(input("Please input num2: "))
